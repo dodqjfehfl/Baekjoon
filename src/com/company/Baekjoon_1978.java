9ce797975bef55abc0ce04ca33be0d4defbc3c86
dodqjfehfl <52148557+dodqjfehfl@users.noreply.github.com>
2020-11-09 13:30:46 +0900

    class02
    11/09

---
+++ src/com/company/Baekjoon_1978.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon_1978 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token=new StringTokenizer(br.readLine());
+
+        int N=Integer.parseInt(token.nextToken());
+        int answer=0;
+
+        token=new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            int num=Integer.parseInt(token.nextToken());
+            if(num<=1)
+                continue;
+
+            boolean flag=true;
+            int limit=(int)Math.sqrt(num)+1;
+            for(int j=2;j<limit;j++){
+                if(num%j==0){
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag)
+                answer++;
+        }
+
+        System.out.println(answer);
+    }
+}
